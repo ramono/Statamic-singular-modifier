6a5be483e6c4ddfae43c48b55d1f768186f5ba79
Ramon Lapenta <ramono@gmail.com>
2014-02-16 11:15:58 +0000

    Added code
    Added add-on files

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Statamic-singular-modifier
+Statamic singular modifier
 ==========================
 
 A modifier add on to singularise variables
+
+This function was ported from an Inflector class found in the Akelos Framework.
+
+### Usage
+	{{ variable|singular }}
---
+++ _add-ons/singular/mod.singular.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Modifier_singular
+ * Converts a variable to singular
+ *
+ * @author  Ramon Lpenta
+ * @author  Yang Yang
+ */
+class Modifier_singular extends Modifier
+{
+
+    function index($value)
+    {
+        $singular = array (
+        '/(quiz)zes$/i' => '\1',
+        '/(matr)ices$/i' => '\1ix',
+        '/(vert|ind)ices$/i' => '\1ex',
+        '/^(ox)en/i' => '\1',
+        '/(alias|status)es$/i' => '\1',
+        '/([octop|vir])i$/i' => '\1us',
+        '/(cris|ax|test)es$/i' => '\1is',
+        '/(shoe)s$/i' => '\1',
+        '/(o)es$/i' => '\1',
+        '/(bus)es$/i' => '\1',
+        '/([m|l])ice$/i' => '\1ouse',
+        '/(x|ch|ss|sh)es$/i' => '\1',
+        '/(m)ovies$/i' => '\1ovie',
+        '/(s)eries$/i' => '\1eries',
+        '/([^aeiouy]|qu)ies$/i' => '\1y',
+        '/([lr])ves$/i' => '\1f',
+        '/(tive)s$/i' => '\1',
+        '/(hive)s$/i' => '\1',
+        '/([^f])ves$/i' => '\1fe',
+        '/(^analy)ses$/i' => '\1sis',
+        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+        '/([ti])a$/i' => '\1um',
+        '/(n)ews$/i' => '\1ews',
+        '/s$/i' => '',
+        );
+
+        $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+
+        $irregular = array(
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves');
+
+        $lowercased_value = strtolower($value);
+        foreach ($uncountable as $_uncountable){
+            if(substr($lowercased_value,(-1*strlen($_uncountable))) == $_uncountable){
+                return $value;
+            }
+        }
+
+        foreach ($irregular as $_plural=> $_singular){
+            if (preg_match('/('.$_singular.')$/i', $value, $arr)) {
+                return preg_replace('/('.$_singular.')$/i', substr($arr[0],0,1).substr($_plural,1), $value);
+            }
+        }
+
+        foreach ($singular as $rule => $replacement) {
+            if (preg_match($rule, $value)) {
+                return preg_replace($rule, $replacement, $value);
+            }
+        }
+
+        return $value;
+    }
+
+
+}
